995bd8bb9c781792ebd713fc1e6f4d5539f7b4d7
Antonis Christofides <anthony@itia.ntua.gr>
2016-11-24 12:48:19 +0200

    Use a template

--- testapp1/views.py
+++ testapp1/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('<p>Hello! This is testapp1 speaking!</p>')
+    return render(request, 'testapp1/index.html')
