b48296155f4648fb3e07c58827a7c9b4cf8f54c6
Antonis Christofides <anthony@itia.ntua.gr>
2016-11-30 11:47:41 +0200

    Add caching

--- testapp1/views.py
+++ testapp1/views.py
@@ -1,5 +1,11 @@
+from django.core.cache import cache
 from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'testapp1/index.html')
+    # Dummy caching commands to verify that cache is working
+    c = cache.get('my_key')
+    if c is None:
+        cache.set('my_key', 'cached')
+        c = 'None'
+    return render(request, 'testapp1/index.html', context={ 'c': c })
